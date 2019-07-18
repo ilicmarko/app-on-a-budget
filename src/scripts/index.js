09bdf65137aa6b6505ebf56e35f7ed4f0a6ceb17
Maverick <ilic.marko.05@live.com>
2019-07-18 11:04:51 +0200

    feat: add some script

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,17 @@
 import '../styles/index.scss';
+import _ from 'lodash';
+
+let arr = [];
+
+for (let i = 1; i < 10000; i++) arr.push({i});
+
+function writeNext(i) {
+    _.map(arr, i => i.i);
+    document.write(i);
+    if(i === 100) return;
+    setTimeout(function() { writeNext(i + 1);}, 3000);
+}
+
+writeNext(1);
 
 console.log('wow such JS much budget');
